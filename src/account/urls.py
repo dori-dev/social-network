cc111ddfe8a22ed2dd61998ff16b74462ff02c2c
dori-dev <mr.dori.dev@gmail.com>
2023-01-05 15:52:30 +0330

    add login, logout and dashboard urls

---
+++ src/account/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'account'
+
+urlpatterns = [
+    path('', views.Dashboard.as_view(), name='dashboard'),
+    path('login/', views.UserLogin.as_view(), name='login'),
+    path('logout/', views.UserLogout.as_view(), name='logout'),
+]
