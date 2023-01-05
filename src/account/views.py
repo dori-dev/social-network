955f610ce6d241660a69a37769fc1c0928436600
dori-dev <mr.dori.dev@gmail.com>
2023-01-05 15:52:15 +0330

    add login, logout and dashboard views

--- src/account/views.py
+++ src/account/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth import views
+from django.contrib.auth.mixins import LoginRequiredMixin
+from . import forms
 
-# Create your views here.
+
+class UserLogin(views.LoginView):
+    form_class = forms.LoginForm
+    template_name = 'account/login.html'
+    redirect_authenticated_user = True
+
+
+class UserLogout(views.LogoutView):
+    template_name = 'account/logged_out.html'
+
+
+class Dashboard(LoginRequiredMixin, generic.TemplateView):
+    template_name = 'account/dashboard.html'
--- src/config/settings.py
+++ src/config/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'account:dashboard'
+LOGIN_URL = 'account:login'
+LOGOUT_URL = 'account:logout'
