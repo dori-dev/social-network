ce59720d438b07a82cbf74fb7e0133a3c5bd8be1
dori-dev <mr.dori.dev@gmail.com>
2023-01-07 13:51:07 +0330

    create profile for user automatically when user created

--- src/account/models.py
+++ src/account/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 User = get_user_model()
 
@@ -17,6 +19,11 @@ class Profile(models.Model):
         upload_to='users/%Y/%m/%d/',
         blank=True,
     )
+
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
 
     def __str__(self):
         return f'{self.user.username}\'s profile'
