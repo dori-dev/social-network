2863be66711dc9ceb89d260738a8a5e363ea1abd
dori-dev <mr.dori.dev@gmail.com>
2023-01-09 10:35:28 +0330

    add image application

--- src/config/settings.py
+++ src/config/settings.py
@@ -36,15 +36,18 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'social_django',
-    'django_extensions',
-    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party apps
+    'social_django',
+    'django_extensions',
+    # local apps
+    'account.apps.AccountConfig',
+    'image.apps.ImageConfig',
 ]
 
 MIDDLEWARE = [
