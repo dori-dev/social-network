895a5e7e890d08e32edd421b111d2d1130b2327f
dori-dev <mr.dori.dev@gmail.com>
2023-01-10 11:06:08 +0330

    add create post view

--- src/post/views.py
+++ src/post/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.shortcuts import redirect
+from . import forms
 
-# Create your views here.
+
+class CreatePost(LoginRequiredMixin, generic.FormView):
+    form_class = forms.CreatePostForm
+    template_name = 'post/create.html'
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        context['form'] = forms.CreatePostForm()
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        form: forms.CreatePostForm = forms.CreatePostForm(
+            request.POST,
+        )
+        if form.is_valid():
+            return self.form_valid(form, **kwargs)
+        return self.form_invalid(form, **kwargs)
+
+    def form_valid(self, form: forms.CreatePostForm, **kwargs):
+        post = form.save(commit=False)
+        post.user = self.request.user
+        post.save()
+        messages.add_message(
+            self.request,
+            messages.SUCCESS,
+            "پست ات با موفقیت منتشر شد :)",
+        )
+        return redirect(post.get_absolute_url())
+
+    def form_invalid(self, form, **kwargs):
+        context = self.get_context_data(**kwargs)
+        context['form'] = form
+        return self.render_to_response(context)
