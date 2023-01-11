7d70a7ac2ddad0ffe6450d8525e1c9a2914a9f1a
dori-dev <mr.dori.dev@gmail.com>
2023-01-11 11:40:04 +0330

    add default profile photo

--- src/post/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'posts'
-
-urlpatterns = [
-    path('create/', views.CreatePost.as_view(), name='create'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='detail'),
-]
