6307d08b7985adc3aa16a97879df2be2a36272b7
dori-dev <mr.dori.dev@gmail.com>
2023-01-13 14:42:03 +0330

    add like path

--- src/post/urls.py
+++ src/post/urls.py
@@ -6,4 +6,5 @@ app_name = 'posts'
 urlpatterns = [
     path('create/', views.CreatePost.as_view(), name='create'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='detail'),
+    path('<slug:slug>/like/', views.LikePost.as_view(), name='like'),
 ]
