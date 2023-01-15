95eb547b4c253433a0f70e1e31f6efb764348216
dori-dev <mr.dori.dev@gmail.com>
2023-01-15 16:09:04 +0330

    add infinite scroll for user list

--- src/contact/views.py
+++ src/contact/views.py
@@ -9,6 +9,10 @@ class UserList(LoginRequiredMixin, generic.ListView):
     queryset = UserModel.objects.filter(
         is_active=True,
     )
-    template_name = 'contact/list.html'
     context_object_name = 'users'
     paginate_by = 24
+
+    def get_template_names(self):
+        if self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            return 'contact/add-users.html'
+        return 'contact/list.html'
