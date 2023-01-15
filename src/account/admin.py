32a0cdd85021b139208a7e7add3c5fab54d49412
dori-dev <mr.dori.dev@gmail.com>
2023-01-15 14:31:10 +0330

    add contact admin

--- src/account/admin.py
+++ src/account/admin.py
@@ -25,3 +25,24 @@ class ProfileAdmin(admin.ModelAdmin):
 @admin.register(models.CustomUser)
 class CustomUserAdmin(UserAdmin):
     pass
+
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = [
+        'user_from',
+        'user_to',
+        'created',
+    ]
+    list_filter = [
+        'created',
+    ]
+    search_fields = [
+        'user_from__username',
+        'user_to__username',
+    ]
+    date_hierarchy = 'created'
+    raw_id_fields = [
+        'user_from',
+        'user_to',
+    ]
