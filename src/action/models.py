b68972cc08ebb62aa1ffa9bc68fabbdaa0aab00f
dori-dev <mr.dori.dev@gmail.com>
2023-01-17 12:06:30 +0330

    add action model

--- src/action/models.py
+++ src/action/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+UserModel = get_user_model()
+
+
+class Action(models.Model):
+    user = models.ForeignKey(
+        UserModel,
+        related_name='actions',
+        db_index=True,
+        on_delete=models.CASCADE,
+    )
+    verb = models.CharField(
+        max_length=256,
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        editable=True,
+    )
+    limit = models.Q(app_label='post', model='post') | \
+        models.Q(app_label='account', model='profile') | \
+        models.Q(app_label='contact', model='contact')
+    target_ct = models.ForeignKey(
+        ContentType,
+        related_name='actions',
+        on_delete=models.CASCADE,
+        limit_choices_to=limit,
+        blank=True,
+        null=True,
+    )
+    target_id = models.PositiveIntegerField(
+        blank=True,
+        null=True,
+        db_index=True,
+    )
+    target = GenericForeignKey(
+        'target_ct',
+        'target_id',
+    )
+
+    class Meta:
+        ordering = (
+            '-created',
+        )
