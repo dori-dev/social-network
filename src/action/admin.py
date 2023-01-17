a733627f309dee222fcfdb49db7f8ec1ffba5372
dori-dev <mr.dori.dev@gmail.com>
2023-01-17 12:06:51 +0330

    add action admin

--- src/action/admin.py
+++ src/action/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Action)
+class ActionAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'verb',
+        'target',
+        'created',
+    ]
+    list_filter = [
+        'created',
+        'target_ct',
+    ]
+    search_fields = [
+        'user__username',
+        'user__username',
+        'verb',
+    ]
+    date_hierarchy = 'created'
+    raw_id_fields = [
+        'user',
+    ]
--- src/contact/admin.py
+++ src/contact/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from . import models
 
+
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = [
