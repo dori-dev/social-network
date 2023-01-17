a7bb4bdf2712fa65ca410967f48a4c2fea43eee6
dori-dev <mr.dori.dev@gmail.com>
2023-01-17 12:06:59 +0330

    add create action func

---
+++ src/action/utils.py
@@ -0,0 +1,29 @@
+import datetime
+from django.utils import timezone
+from django.contrib.contenttypes.models import ContentType
+from . import models
+
+
+def create_action(user, verb, target=None):
+    now = timezone.now()
+    last_hour = now - datetime.timedelta(hours=1)
+    similar_actions = models.Action.objects.filter(
+        user=user,
+        verb=verb,
+        created__gte=last_hour,
+    )
+    if target is not None:
+        target_ct = ContentType.objects.get_for_model(target)
+        similar_action = similar_actions.filter(
+            target_ct=target_ct,
+            target_id=target.id,
+        )
+    if not similar_action.exists():
+        action = models.Action(
+            user=user,
+            verb=verb,
+            target=target,
+        )
+        action.save()
+        return True
+    return False
