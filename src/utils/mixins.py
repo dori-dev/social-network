34f90abe821dc914f07c8b35f79c7feadf759ff8
dori-dev <mr.dori.dev@gmail.com>
2023-01-20 20:08:16 +0330

    add ip to views counter

--- src/utils/mixins.py
+++ src/utils/mixins.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseBadRequest
-from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpRequest
 from django.conf import settings
 import redis
 
@@ -11,7 +11,7 @@ r = redis.Redis(
 
 
 class AjaxRequiredMixin:
-    def dispatch(self, request: WSGIRequest, *args, **kwargs):
+    def dispatch(self, request: HttpRequest, *args, **kwargs):
         if request.headers.get('x-requested-with') == 'XMLHttpRequest':
             return super().dispatch(request, *args, **kwargs)
         return HttpResponseBadRequest()
@@ -19,10 +19,17 @@ class AjaxRequiredMixin:
 
 class ViewCounterMixin:
     def get_context_data(self, **kwargs):
+        request: HttpRequest = self.request
         context = super().get_context_data(**kwargs)
+        x_forwarded_for: str = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip_address = x_forwarded_for.split(',')[0]
+        else:
+            ip_address = request.META.get('REMOTE_ADDR')
         slug = context['post'].slug
-        context["total_views"] = r.incr(
-            f'post:{slug}:total_views',
-            amount=1,
+        r.sadd(
+            f'post:{slug}:view_ips',
+            ip_address,
         )
+        context["total_views"] = r.scard(f'post:{slug}:view_ips')
         return context
