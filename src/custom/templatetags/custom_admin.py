80c733c051cd7afe230f2f9277052f27acdce4b0
dori-dev <mr.dori.dev@gmail.com>
2023-01-26 16:37:42 +0330

    remove extra view from admin

--- src/custom/templatetags/custom_admin.py
+++ src/custom/templatetags/custom_admin.py
@@ -1,7 +1,3 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
 import re
 from django import template
 from django.utils.html import format_html
@@ -10,7 +6,8 @@ from django.utils.safestring import mark_safe
 from django.contrib.admin.views.main import (PAGE_VAR)
 
 register = template.Library()
-assignment_tag = register.assignment_tag if hasattr(register, 'assignment_tag') else register.simple_tag
+assignment_tag = register.assignment_tag if hasattr(
+    register, 'assignment_tag') else register.simple_tag
 
 
 @register.filter
@@ -28,38 +25,6 @@ def checkbox(value):
 @assignment_tag(takes_context=True)
 def admin_get_menu(context):
     return get_menu_items(context)
-
-
-@assignment_tag(takes_context=True)
-def get_direction(context):
-    res = {
-        'panel': 'text-left',
-        'notify': 'right',
-        'float': 'float-right',
-        'reverse_panel': 'text-right',
-        'nav': 'ml-auto'
-    }
-
-    if context.get('LANGUAGE_BIDI'):
-        res['panel'] = 'text-right'
-        res['notify'] = 'left'
-        res['float'] = ''
-        res['reverse_panel'] = 'text-left'
-        res['nav'] = 'mr-auto'
-    return res
-
-
-@assignment_tag(takes_context=True)
-def get_admin_setting(context):
-    # user = context.get('request').user
-    # admin_black_setting = user.admin_black_setting if hasattr(user, 'admin_black_setting') else None
-    res = {
-        # 'sidebar_background': admin_black_setting.sidebar_background if admin_black_setting else 'primary',
-        # 'dark_mode': admin_black_setting.dark_mode if admin_black_setting else True,
-        # 'input_bg_color': '#ffffff' if admin_black_setting and not admin_black_setting.dark_mode else '#27293c'
-    }
-
-    return res
 
 
 @register.simple_tag
