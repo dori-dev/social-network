a55f6b19ed6931fd686a93f5bef93d4d6802c420
dori-dev <mr.dori.dev@gmail.com>
2023-03-25 00:04:29 +0330

    run process task using subprocess

--- src/config/settings/__init__.py
+++ src/config/settings/__init__.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from .base import *
 
 if os.getenv('STATE', 'development') == 'production':
@@ -7,3 +8,7 @@ if os.getenv('STATE', 'development') == 'production':
 else:
     from .development import *
     print('development state')
+
+if os.environ.get('process_tasks') is None:
+    os.environ['process_tasks'] = 'running'
+    subprocess.Popen(['python', 'manage.py', 'process_tasks'])
