d80c7bf6142f15411e0574771caef694ffc73c77
dori-dev <mr.dori.dev@gmail.com>
2023-03-24 20:44:14 +0330

    serve static & media files on production

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,9 +1,11 @@
 """social network URL Configuration
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('home.urls', namespace='home')),
@@ -25,3 +27,17 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT,
     )
+    urlpatterns += staticfiles_urlpatterns()
+else:
+    urlpatterns += [
+        re_path(
+            r'^media/(?P<path>.*)$',
+            serve,
+            {'document_root': settings.MEDIA_ROOT}
+        ),
+        re_path(
+            r'^static/(?P<path>.*)$',
+            serve,
+            {'document_root': settings.STATIC_ROOT}
+        ),
+    ]
