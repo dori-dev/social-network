39da2900e54b60184bf3f9b2652668412f207141
dori-dev <mr.dori.dev@gmail.com>
2023-04-16 18:06:58 +0330

    load environment variable in init

--- src/config/settings/__init__.py
+++ src/config/settings/__init__.py
@@ -1,6 +1,21 @@
 import os
+from pathlib import Path
 import subprocess
+import dotenv
 from .base import *
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
+DOT_ENV_PATH = BASE_DIR / '.env'
+if DOT_ENV_PATH.exists():
+    dotenv.read_dotenv(str(DOT_ENV_PATH))
+else:
+    print(
+        "No .env found, be sure to make it.\n"
+        "You can rename .env.example file to .env and "
+        "set your environ variable in it."
+    )
 
 if os.getenv('STATE', 'development') == 'production':
     from .production import *
